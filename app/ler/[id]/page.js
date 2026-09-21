706a8e42d923301e792419740c0995a5b6ba4bcf
Samiris Melo <sazinhamelo@gmail.com>
2026-09-21 16:51:14 -0700

    Fix Supabase import in reader

--- app/ler/[id]/page.js
+++ app/ler/[id]/page.js
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import {useEffect,useRef,useState} from "react";
-import {createClient} from "../../lib/supabase/client";
+import {createClient} from "../../../lib/supabase/client";
 
 export default function Reader({params}){
  const supabase=createClient();
